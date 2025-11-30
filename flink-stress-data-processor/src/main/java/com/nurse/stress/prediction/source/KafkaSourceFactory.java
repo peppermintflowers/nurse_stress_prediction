0199b89ef82369dd55aa4a921d876f3f7d67d218
Komal <mrig@mrig.local>
2025-11-30 18:01:44 -0500

    add starting offset earliest and commit offset on checkpointing

--- flink-stress-data-processor/src/main/java/com/nurse/stress/prediction/source/KafkaSourceFactory.java
+++ flink-stress-data-processor/src/main/java/com/nurse/stress/prediction/source/KafkaSourceFactory.java
@@ -3,6 +3,7 @@ package com.nurse.stress.prediction.source;
 import com.nurse.stress.prediction.SensorRecord;
 import com.nurse.stress.prediction.processing.Constants;
 import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.formats.avro.AvroDeserializationSchema;
 
 public class KafkaSourceFactory {
@@ -12,6 +13,8 @@ public class KafkaSourceFactory {
         return KafkaSource.<SensorRecord>builder()
                 .setBootstrapServers(brokers)
                 .setTopics(topics)
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setProperty("commit.offsets.on.checkpoint", "true")
                 .setGroupId(Constants.GROUP_ID)
                 .setValueOnlyDeserializer(AvroDeserializationSchema.forSpecific(SensorRecord.class))
                 .build();
