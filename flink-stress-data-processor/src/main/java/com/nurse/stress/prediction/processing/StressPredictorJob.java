deaa6133078fa11f8726e9443c5482851eed561e
Komal <mrig@mrig.local>
2025-11-30 17:53:31 -0500

    Enabled checkpointing and added logging in the job

--- flink-stress-data-processor/src/main/java/com/nurse/stress/prediction/processing/StressPredictorJob.java
+++ flink-stress-data-processor/src/main/java/com/nurse/stress/prediction/processing/StressPredictorJob.java
@@ -2,9 +2,9 @@ package com.nurse.stress.prediction.processing;
 
 import com.nurse.stress.prediction.SensorRecord;
 import com.nurse.stress.prediction.model.IOTPing;
-import com.nurse.stress.prediction.model.NurseMetrics;
 import com.nurse.stress.prediction.sink.InfluxSinkPing;
 import com.nurse.stress.prediction.source.KafkaSourceFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -12,29 +12,27 @@ import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindo
 
 import java.time.Duration;
 
-
+@Slf4j
 public class StressPredictorJob {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(2);
+        env.enableCheckpointing(5000);
 
         KafkaSource<SensorRecord> source = KafkaSourceFactory.create(Constants.BROKER_URL, Constants.TOPIC_NAME);
 
         DataStream<SensorRecord> records = env.fromSource(source,WatermarkStrategyFactory.create(),"Kafka Source");
 
         DataStream<IOTPing> predictedStress = records.keyBy(r->r.getId())
-                .window(TumblingEventTimeWindows.of(Duration.ofMinutes(1)))
+                .window(TumblingEventTimeWindows.of(Duration.ofSeconds(5)))
                 .aggregate(new AverageAggregator(),new WindowResultFunction())
                 .name("Stress Prediction 1 minute batch")
-                .map(n -> new IOTPing(
-                        n.getId(),
-                        n.getEDA(),
-                        n.getHR(),
-                        n.getTEMP(),
-                        n.getDatetime(),
-                        0
-                ));
+                .map(n -> {
+                    IOTPing ping = new IOTPing(n.getId(), n.getEDA(), n.getHR(), n.getTEMP(), n.getDatetime(), 0);
+                    log.info("Predicted ping: {}", ping);
+                    return ping;
+                });
 
         predictedStress.addSink(new InfluxSinkPing());
         env.execute("Flink Streaming Metrics Example");
